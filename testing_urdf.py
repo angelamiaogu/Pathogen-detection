8fa433679d1dae51aff6802801dd4984e019dcae
Andy <mimi791202@gmail.com>
2025-01-08 20:59:17 +0900

    makign arm

--- testing_urdf.py
+++ testing_urdf.py
@@ -7,9 +7,12 @@ p.connect(p.GUI) # connect to GUI
 urdf_adress = pybullet_data.getDataPath() # get address of the urdf
 p.setAdditionalSearchPath(urdf_adress) # set additional search path, having all urdf buildin
 planeId = p.loadURDF("plane.urdf")
+cutebox = p.loadURDF("cute_box.urdf",[0,0,1])
 
+p.setGravity(0,0,-10)
 
 
 for i in range(10000):
     time.sleep(0.01)
     p.stepSimulation()
+    
